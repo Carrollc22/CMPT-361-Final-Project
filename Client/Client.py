641bcaf82645d74eaebd2b3227566e886aac62f9
Carrollc22 <Carrollc22@mymacewan.ca>
2025-03-28 11:45:22 -0600

    Update login process to encrypt/decrypt with server public and private keys.

--- Client/Client.py
+++ Client/Client.py
@@ -1,10 +1,66 @@
+<<<<<<< Updated upstream
 import socket
+=======
+import socket 
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+from Crypto.Util.Padding import pad
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import json
+import os
+
+# load_public_key
+# save the key to a variable from a .pem file
+# param: key_file. The file containing the key
+# returns: public_key. The read key from the file.
+def load_key(pem_file):
+    # open and read pem file
+    with open(pem_file, "rb") as key_file:
+        # save key file to key
+        key = RSA.import_key(key_file.read())
+    return key
+
+# encryption
+# encrypt the message
+# params: data, key
+# return: encryped_data
+def encryption(data, public_key):
+    # init cipher with public key
+    cipher_rsa = PKCS1_OAEP.new(public_key)
+    
+    # encrypt data with cipher
+    encrypted_data = cipher_rsa.encrypt(data)
+    
+    return encrypted_data
+>>>>>>> Stashed changes
+
+# decryption
+# decrypt the incoming message
+# Parameters: encrypted_data, key
+# Returns: decrypted_data.decoode
+def decryption(encrypted_data, private_key):
+    # Init cipher with private key
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    
+    # Decrypt data
+    decrypted_data = cipher_rsa.decrypt(encrypted_data)
+    
+    # return decoded data
+    return decrypted_data.decode('utf-8')
 
 # start_client
 # connects to server through known server ip and port
 # params: server_ip, server_port. known ip and port addresses to start connection 
 # return: none
 def start_client(server_ip, server_port):
+<<<<<<< Updated upstream
+=======
+    # get the server public key form server_public key file
+    pem_file = "Client/server_public.pem"
+    key = load_key(pem_file)
+
+>>>>>>> Stashed changes
     # config socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,8 +70,24 @@ def start_client(server_ip, server_port):
     # temporary server and client communication!!!
 
     welcome_message = client_socket.recv(1024).decode()
-    print(welcome_message)
 
+<<<<<<< Updated upstream
+=======
+    # Login Block
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    data = (username + "," + password).encode('utf-8')
+    encrypted_creds = encryption(data, key)
+    client_socket.send(encrypted_creds)
+    response = client_socket.recv(1024).decode()
+
+    if response == "success":
+        print("Select the operation:")
+    else:
+        print("Invalid username or password.")
+        print("Terminating.")
+
+>>>>>>> Stashed changes
     while True:
         # user input
         message = input("You: ")
--- Server/Server.py
+++ Server/Server.py
@@ -1,5 +1,63 @@
 import socket 
 import threading 
+<<<<<<< Updated upstream
+=======
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+from Crypto.Util.Padding import pad
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import hashlib
+import json
+import os
+
+# load_public_key
+# save the key to a variable from a .pem file
+# param: key_file. The file containing the key
+# returns: public_key. The read key from the file.
+def load_key(pem_file):
+    with open(pem_file, "rb") as key_file:
+        key = RSA.import_key(key_file.read())
+    return key
+
+# encryption
+# encrypt the message
+# params: username, password, key
+# return: encryped_data
+def encryption(username, password, public_key):
+    data = (username + "," + password).encode('utf-8')
+    padded_data = pad(data, AES.block_size)
+    
+    cipher = AES.new(public_key, AES.MODE_ECB)
+    encrypted_data = cipher.encrypt(padded_data)
+    return encrypted_data
+
+# decryption
+# decrypt the incoming message
+# Parameters: encrypted_data, key
+# Returns: unpadded_data.decoode
+def decryption(encrypted_message, private_key):
+    # Initialize the RSA cipher with the private key
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    
+    # Decrypt the encrypted message
+    decrypted_message = cipher_rsa.decrypt(encrypted_message)
+    
+    # Return the decrypted message (it will be in bytes, so we decode it to a string)
+    return decrypted_message.decode('utf-8')
+
+# validation
+# validate whether or not the username and password are acceptable
+# Parameters: username, password
+# Returns: True
+def validation(username, password):
+    with open('Server/user_pass.json', 'r') as file:
+            valid_clients = json.load(file)
+
+    if username in valid_clients and valid_clients[username] == password:
+        return True
+    return False
+>>>>>>> Stashed changes
 
 # Get_server_ip 
 # find the ip of the machine running the server program. Allows for a dynamic ip address when running the server. 
@@ -15,8 +73,6 @@ def get_server_ip():
     # find local ip 
     server_ip = s.getsockname()[0] 
 
-    print(server_ip)
-
     # close socket and return local ip
     s.close()
     return server_ip
@@ -26,14 +82,39 @@ def get_server_ip():
 # parameters: client_socket. The socket connection between server and client.
 # returns: none
 def handle_client(client_socket, client_address):
+<<<<<<< Updated upstream
+=======
+    # AES encryption key
+    pem_file = "Server/server_private.pem"
+    key = load_key(pem_file)
+>>>>>>> Stashed changes
 
     # temporary client handling!!!!!!
 
     client_socket.send(b"Hello from the server!")
 
+<<<<<<< Updated upstream
     client_message = client_socket.recv(1024).decode("utf-8")
     print("Received from client: ", client_message)
+=======
+    # Receive encrypted data
+    encrypted_data = client_socket.recv(1024)
 
+    # Decrypt encrypted data
+    decrypted_data = decryption(encrypted_data, key)
+
+    # Split the data
+    username, password = decrypted_data.split(',')
+
+    if validation(username, password):
+        client_socket.send("success".encode())
+        print("Connection Accepted and Symmetric Key Generated for client:", username)
+    else:
+        client_socket.send("fail".encode())
+        print("The received client information:", username, "is invalid (Connection Terminated).")
+>>>>>>> Stashed changes
+
+    
     client_socket.close()
 
 # start_server
@@ -44,14 +125,12 @@ def start_server():
     # Get server machine ip
     server_ip = get_server_ip()
 
-    print("Server started on IP: ", server_ip)
+    print("The server is ready to accept connections")
 
     # Create a socket and bind to the machine ip and port 13000
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((server_ip, 13000))  
     server_socket.listen(5)
-
-    print("Listening on ", server_ip, ":13000 for incoming connections...")
 
     # Accept incoming client connections
     while True:
