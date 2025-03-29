d3ae97ea642101f43d11424184b426b621bf15a4
Carrollc22 <Carrollc22@mymacewan.ca>
2025-03-28 21:36:17 -0600

    Merged together me and sams branches
    project is complete up to subprotocols

--- Client/Client.py
+++ Client/Client.py
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-import socket
-=======
 import socket 
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
@@ -10,7 +7,7 @@ from Crypto.Cipher import PKCS1_OAEP
 import json
 import os
 
-# load_public_key
+# load_key
 # save the key to a variable from a .pem file
 # param: key_file. The file containing the key
 # returns: public_key. The read key from the file.
@@ -21,11 +18,11 @@ def load_key(pem_file):
         key = RSA.import_key(key_file.read())
     return key
 
-# encryption
-# encrypt the message
-# params: data, key
+# encryptionRSA
+# encrypt the message with RSA for assymetric keys
+# params: data, public_key
 # return: encryped_data
-def encryption(data, public_key):
+def encryptionRSA(data, public_key):
     # init cipher with public key
     cipher_rsa = PKCS1_OAEP.new(public_key)
     
@@ -33,13 +30,23 @@ def encryption(data, public_key):
     encrypted_data = cipher_rsa.encrypt(data)
     
     return encrypted_data
->>>>>>> Stashed changes
 
-# decryption
-# decrypt the incoming message
+# encryptionAES
+# encrypt the message with AES for symmetric keys
+# params: data, sym_key
+# returns: encrypted_data
+def encryptionAES(data, sym_key):
+    padded_data = pad(data, AES.block_size)
+    
+    cipher = AES.new(sym_key, AES.MODE_ECB)
+    encrypted_data = cipher.encrypt(padded_data)
+    return encrypted_data
+
+# decryptionRSA
+# decrypt the incoming message with rsa
 # Parameters: encrypted_data, key
 # Returns: decrypted_data.decoode
-def decryption(encrypted_data, private_key):
+def decryptionRSA(encrypted_data, private_key):
     # Init cipher with private key
     cipher_rsa = PKCS1_OAEP.new(private_key)
     
@@ -49,63 +56,106 @@ def decryption(encrypted_data, private_key):
     # return decoded data
     return decrypted_data.decode('utf-8')
 
+# decryptionAES
+# decrypt the incoming message with aes
+# Parameters: encrypted_data, key
+# Returns: unpadded_data.decoode
+def decryptionAES(encrypted_data, sym_key):
+    # init cipher 
+    cipher = AES.new(sym_key, AES.MODE_ECB)
+
+    # decrypt
+    decrypted_data = cipher.decrypt(encrypted_data)
+    
+    #unpad
+    unpadded_data = unpad(decrypted_data, AES.block_size)
+    return unpadded_data.decode('utf-8')
+
 # start_client
 # connects to server through known server ip and port
 # params: server_ip, server_port. known ip and port addresses to start connection 
 # return: none
 def start_client(server_ip, server_port):
-<<<<<<< Updated upstream
-=======
     # get the server public key form server_public key file
     pem_file = "Client/server_public.pem"
     key = load_key(pem_file)
 
->>>>>>> Stashed changes
     # config socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect to the server
     client_socket.connect((server_ip, server_port))
 
-    # temporary server and client communication!!!
+    client_socket.recv(1024).decode()
 
-    welcome_message = client_socket.recv(1024).decode()
-
-<<<<<<< Updated upstream
-=======
-    # Login Block
+    # login block
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     data = (username + "," + password).encode('utf-8')
-    encrypted_creds = encryption(data, key)
+    encrypted_creds = encryptionRSA(data, key)
     client_socket.send(encrypted_creds)
+    
+    # check if valid login
     response = client_socket.recv(1024).decode()
-
-    if response == "success":
-        print("Select the operation:")
-    else:
+    if response == "Invalid username or password":
         print("Invalid username or password.")
         print("Terminating.")
+        client_socket.close()
+        return
+    
+    # receive encrypted key
+    encrypted_sym_key = client_socket.recv(1024)
 
->>>>>>> Stashed changes
+    # load client's private key and decrypt sym_key
+    client_private_file = f"Client/{username}_private.pem"
+    client_private_key = load_key(client_private_file)
+    cipher_rsa = PKCS1_OAEP.new(client_private_key)
+    sym_key = cipher_rsa.decrypt(encrypted_sym_key)
+
+    # send ok with sym_key
+    message = encryptionAES("OK".encode('utf-8'), sym_key)
+    client_socket.send(message)
+
     while True:
-        # user input
-        message = input("You: ")
-        client_socket.send(message.encode())
-
-        # If the user types "exit", close the connection
-        if message.lower() == "exit":
-            print("Closing connection...")
-            break
-
-        # receive/print server response
-        server_response = client_socket.recv(1024).decode()
-        print("Server: ", server_response)
-
+ 
+            # receive encrypted menu
+            encrypted_menu = client_socket.recv(1024)
+             
+            # decrypt and display menu
+            menu = decryptionAES(encrypted_menu, sym_key)
+            print(menu, end="")
+             
+            # get user choice
+            choice = input()
+             
+            # encrypt and send choice
+            encrypted_choice = encryptionAES(choice.encode("utf-8"), sym_key)
+            client_socket.send(encrypted_choice)
+             
+             
+            if choice == '1':
+                print("Sending email subprotocol")
+                 
+            elif choice == '2':
+                print("Viewing inbox subprotocol")
+                 
+            elif choice == '3':
+ 
+                # IMPLEMENT VIEW EMAIL SUBPROTOCOL
+                 
+                print("Viewing email subprotocol")
+                 
+            elif choice == '4':
+                print("The connection is terminated with the server.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+                 
     # Close the client socket
     client_socket.close()
 
-# Run the client
-# server_ip = CHANGE TO YOUR DEVICES IP
-server_port = 13000
-start_client(server_ip, server_port)
+# run client
+if __name__ == "__main__":
+    server_ip = "127.0.0.1"
+    server_port = 13000
+    start_client(server_ip, server_port)
--- Server/Server.py
+++ Server/Server.py
@@ -1,17 +1,20 @@
 import socket 
 import threading 
-<<<<<<< Updated upstream
-=======
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from Crypto.Util.Padding import pad
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-import hashlib
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 import json
 import os
 
-# load_public_key
+
+MENU = "Select the operation:\n1) Create and send an email\n2) Display the inbox list\n3) Display the email contents\n4) Terminate the connection\nchoice: "
+ 
+ 
+# load_key
 # save the key to a variable from a .pem file
 # param: key_file. The file containing the key
 # returns: public_key. The read key from the file.
@@ -20,31 +23,56 @@ def load_key(pem_file):
         key = RSA.import_key(key_file.read())
     return key
 
-# encryption
-# encrypt the message
-# params: username, password, key
+# encryptionRSA
+# encrypt the message with RSA for assymetric keys
+# params: data, public_key
 # return: encryped_data
-def encryption(username, password, public_key):
-    data = (username + "," + password).encode('utf-8')
+def encryptionRSA(data, public_key):
+    # init cipher with public key
+    cipher_rsa = PKCS1_OAEP.new(public_key)
+    
+    # encrypt data with cipher
+    encrypted_data = cipher_rsa.encrypt(data)
+    
+    return encrypted_data
+
+# encryptionAES
+# encrypt the message with AES for symmetric keys
+# params: data, sym_key
+# returns: encrypted_data
+def encryptionAES(data, sym_key):
     padded_data = pad(data, AES.block_size)
     
-    cipher = AES.new(public_key, AES.MODE_ECB)
+    cipher = AES.new(sym_key, AES.MODE_ECB)
     encrypted_data = cipher.encrypt(padded_data)
     return encrypted_data
 
-# decryption
+# decryptionRSA
 # decrypt the incoming message
 # Parameters: encrypted_data, key
-# Returns: unpadded_data.decoode
-def decryption(encrypted_message, private_key):
-    # Initialize the RSA cipher with the private key
+# Returns: decrypted_data.decoode
+def decryptionRSA(encrypted_data, private_key):
+    # Init cipher with private key
     cipher_rsa = PKCS1_OAEP.new(private_key)
     
-    # Decrypt the encrypted message
-    decrypted_message = cipher_rsa.decrypt(encrypted_message)
+    # Decrypt data
+    decrypted_data = cipher_rsa.decrypt(encrypted_data)
     
-    # Return the decrypted message (it will be in bytes, so we decode it to a string)
-    return decrypted_message.decode('utf-8')
+    # return decoded data
+    return decrypted_data.decode('utf-8')
+
+# decryptionAES
+# decrypt the username and password
+# Parameters: encrypted_data, key
+# Returns: unpadded_data.decoode
+def decryptionAES(encrypted_data, sym_key):
+    # create an AES cipher object with the key in ECB mode
+    cipher = AES.new(sym_key, AES.MODE_ECB)
+
+    decrypted_data = cipher.decrypt(encrypted_data)
+
+    unpadded_data = unpad(decrypted_data, AES.block_size)
+    return unpadded_data.decode('utf-8')
 
 # validation
 # validate whether or not the username and password are acceptable
@@ -57,7 +85,6 @@ def validation(username, password):
     if username in valid_clients and valid_clients[username] == password:
         return True
     return False
->>>>>>> Stashed changes
 
 # Get_server_ip 
 # find the ip of the machine running the server program. Allows for a dynamic ip address when running the server. 
@@ -82,39 +109,95 @@ def get_server_ip():
 # parameters: client_socket. The socket connection between server and client.
 # returns: none
 def handle_client(client_socket, client_address):
-<<<<<<< Updated upstream
-=======
-    # AES encryption key
     pem_file = "Server/server_private.pem"
     key = load_key(pem_file)
->>>>>>> Stashed changes
-
-    # temporary client handling!!!!!!
 
     client_socket.send(b"Hello from the server!")
 
-<<<<<<< Updated upstream
-    client_message = client_socket.recv(1024).decode("utf-8")
-    print("Received from client: ", client_message)
-=======
-    # Receive encrypted data
+    # Receive encrypted username/password
     encrypted_data = client_socket.recv(1024)
 
-    # Decrypt encrypted data
-    decrypted_data = decryption(encrypted_data, key)
+    # Decrypt encrypted username/password
+    decrypted_data = decryptionRSA(encrypted_data, key)
 
-    # Split the data
+    # Split 
     username, password = decrypted_data.split(',')
 
+    # validate with json file and get public key
     if validation(username, password):
+        # generate sym key for future aes encrytion
+        sym_key = get_random_bytes(32)
+        
+        # get user public key for rsa
+        if username == "client1":
+            pem_file = "Server/Client1/client1_public.pem"
+        elif username == "client2":
+            pem_file = "Server/Client2/client2_public.pem"
+        elif username == "client3":
+            pem_file = "Server/Client3/client3_public.pem"
+        elif username == "client4":
+            pem_file = "Server/Client4/client4_public.pem"
+        elif username == "client5":
+            pem_file = "Server/Client5/client5_public.pem"
+        key = load_key(pem_file)
+
+        # success 
         client_socket.send("success".encode())
+        client_socket.send(encryptionRSA(sym_key, key))
         print("Connection Accepted and Symmetric Key Generated for client:", username)
     else:
-        client_socket.send("fail".encode())
+        # fail
+        client_socket.send("Invalid username or password".encode())
         print("The received client information:", username, "is invalid (Connection Terminated).")
->>>>>>> Stashed changes
+        return
 
+    # ok message
+    encrypted_data = client_socket.recv(1024)
+    message = decryptionAES(encrypted_data, sym_key)
+
+    if message != "OK":
+        return
     
+            # menu loop
+    while True:
+ 
+        # encrypt menu and send to client
+        encrypted_menu = encryptionAES(MENU.encode("utf-8"), sym_key)
+        client_socket.send(encrypted_menu)
+ 
+        # receive encrypted choice from client
+        encrypted_choice = client_socket.recv(1024)
+        choice = decryptionAES(encrypted_choice, sym_key)
+ 
+        if choice == '1':
+                     
+                # IMPLEMENT SEND EMAIL SUBPROTOCOL
+ 
+                print(f"Client {username} selected: Send email")
+                     
+                     
+        elif choice == '2':
+ 
+            # IMPLEMENT VIEW INBOX SUBPROTOCOL
+ 
+            print(f"Client {username} selected: View inbox")
+                 
+                 
+        elif choice == '3':
+ 
+            # IMPLEMENT VIEW EMAIL SUBPROTOCOL
+ 
+            print(f"Client {username} selected: View email content")
+                 
+                 
+        elif choice == '4':
+ 
+            # terminate connection
+ 
+            print(f"Terminating connection with {username}.")
+            break
+        else:
+            print(f"Invalid choice received from {username}: {choice}")
     client_socket.close()
 
 # start_server
@@ -129,7 +212,7 @@ def start_server():
 
     # Create a socket and bind to the machine ip and port 13000
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((server_ip, 13000))  
+    server_socket.bind(("127.0.0.1", 13000))  
     server_socket.listen(5)
 
     # Accept incoming client connections
@@ -141,7 +224,8 @@ def start_server():
         client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
         client_thread.start()
 
-start_server()
+if __name__ == "__main__":
+     start_server()
 
 
 
